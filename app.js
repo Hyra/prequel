0a0eca098f7dacdd92be90cf62f37627d0e4684b
Hyra <stef@mindthecode.com>
2018-03-18 12:34:39 +0100

    Memory cache

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ server.use(prerender.sendPrerenderHeader());
 // server.use(prerender.blockResources());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
-// server.use(prerender.inMemoryHtmlCache());
+server.use(prerender.inMemoryHtmlCache());
 // server.use(prerender.s3HtmlCache());
 
 // server.use(require('prerender-redis-cache'));
